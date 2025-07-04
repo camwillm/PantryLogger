56511f114973d1a2365f2a08553e1ef88e80555c
camwillm <camwillmofficial@gmail.com>
2025-07-04 05:11:15 -0400

    Initial Commit

---
+++ PantryLogger/PantryLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryLoggerApp.swift
+//  PantryLogger
+//
+//  Created by Cameron Williams on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PantryLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PantryLoggerTests/PantryLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PantryLoggerTests.swift
+//  PantryLoggerTests
+//
+//  Created by Cameron Williams on 7/4/25.
+//
+
+import Testing
+@testable import PantryLogger
+
+struct PantryLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
